898f2f33ab1cc78488651e127a71445191d9151b
kareem <kareemhamed@gmail.com>
2023-11-13 09:14:38 +0200

    first commit

---
+++ home.js
@@ -0,0 +1,33 @@
+
+let p=true
+let l=11
+let nn='11'
+let oo="popo"
+
+switch(nn){
+    case '1':
+        console.log(515245)
+        break;
+    case '14':
+        console.log(44444444444)
+        break;
+    case '1l1':
+        console.log(101010101)
+        break;
+
+        default :
+        console.log(99999999)
+
+
+}
+
+
+
+
+
+
+
+
+let oop = "kaareem hamed el meshad"
+
+console.log(oop.slice(1  ))
